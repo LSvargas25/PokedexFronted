7a5aee734d30e94dcd04e7965c6e040db4322cd8
Luis Steven Vargas <vargasrodriguezluissteven@gmail.com>
2025-10-15 15:19:51 -0600

    10/15/2025 commit

--- src/app/Components/Pokedex/pokedex/pokedex.ts
+++ src/app/Components/Pokedex/pokedex/pokedex.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AScreen } from '../../Screens/ascreen/ascreen';
 
 @Component({
   selector: 'app-pokedex',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, AScreen],
   templateUrl: './pokedex.html',
-  styleUrl: './pokedex.scss'
+  styleUrls: ['./pokedex.scss']
 })
-export class Pokedex {
-
-}
+export class Pokedex {}
--- src/app/Components/Screens/ascreen/ascreen.ts
+++ src/app/Components/Screens/ascreen/ascreen.ts
@@ -1,28 +1,102 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ascreen',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './ascreen.html',
-  styleUrl: './ascreen.scss'
+  styleUrls: ['./ascreen.scss']
 })
-export class AScreen {
- isOn = false;
-  showWelcome = false;
+export class AScreen implements AfterViewInit {
+  @ViewChild('videoPlayer') videoPlayer!: ElementRef<HTMLVideoElement>;
+
+  isOn = false;
+  showMenu = false;
+
+  currentVideo = 'assets/videos/intro.mp4';
   options = ['Poked', 'Pokémon Search', 'Trainer Info', 'Settings'];
+
+  // 🎚️ Control de volumen
+  volume: number = 0.5;
+  isMuted: boolean = false;
+  volumeIcon: string = '🔉'; // icono inicial
+
+  ngAfterViewInit() {
+    this.playVideo();
+    if (this.videoPlayer) {
+      this.videoPlayer.nativeElement.volume = this.volume;
+    }
+    this.updateVolumeIcon();
+  }
 
   powerOn() {
     this.isOn = true;
-    this.showWelcome = true;
-    setTimeout(() => {
-      this.showWelcome = false;
-    }, 3000);
+    this.showMenu = false;
+    this.currentVideo = 'assets/videos/intro.mp4';
+    setTimeout(() => this.playVideo(), 0);
   }
 
   powerOff() {
     this.isOn = false;
-    this.showWelcome = false;
+    this.showMenu = false;
+  }
+
+  private playVideo() {
+    if (this.videoPlayer) {
+      const video = this.videoPlayer.nativeElement;
+      video.currentTime = 0;
+      video.volume = this.volume;
+      video.muted = this.isMuted;
+      video.play();
+    }
+  }
+
+  onVideoEnded() {
+    if (!this.showMenu) {
+      this.showMenu = true;
+      this.currentVideo = 'assets/videos/pikachu.mp4';
+      setTimeout(() => this.playVideo(), 0);
+    }
+  }
+
+  onOptionClick(option: string) {
+    console.log('Seleccionaste:', option);
+  }
+
+  toggleMute() {
+    this.isMuted = !this.isMuted;
+    this.videoPlayer.nativeElement.muted = this.isMuted;
+    this.updateVolumeIcon();
+  }
+
+  changeVolume() {
+    const video = this.videoPlayer.nativeElement;
+    video.volume = this.volume;
+    this.isMuted = this.volume === 0;
+    video.muted = this.isMuted;
+    this.updateVolumeIcon(true);
+  }
+
+  // 🌀 Actualiza el ícono según el volumen
+  private updateVolumeIcon(animated: boolean = false) {
+    if (this.isMuted || this.volume === 0) {
+      this.volumeIcon = '🔇';
+    } else if (this.volume < 0.3) {
+      this.volumeIcon = '🔈';
+    } else if (this.volume < 0.7) {
+      this.volumeIcon = '🔉';
+    } else {
+      this.volumeIcon = '🔊';
+    }
+
+    if (animated) {
+      const icon = document.querySelector('.volume-icon');
+      if (icon) {
+        icon.classList.add('bounce');
+        setTimeout(() => icon.classList.remove('bounce'), 300);
+      }
+    }
   }
 }
